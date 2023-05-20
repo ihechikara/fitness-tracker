a571de1198e5dafda580d12b31885079327d4f22
Ihechikara <ihechivinabba@gmail.com>
2023-05-20 20:14:15 +0100

    feat: fetch all workouts
    - fetch workouts from database
    - render workouts in Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react"
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails"
+
 function Home() {
-  return (
-    <div>
-        <h1>Home</h1>
-    </div>
-  )
+    const [workouts, setWorkouts] = useState(null)
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+
+            try {
+                const response =  await fetch("/api/workouts")
+                const json = await response.json()
+    
+                if(response.ok){
+                    setWorkouts(json)
+                }
+                
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        fetchWorkouts()
+    }, [])
+
+
+    return (
+        <div className="home">
+            <div className="workouts">
+                { workouts && workouts.map((workout) => (
+                    <WorkoutDetails key={workout._id} workout={workout}/>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default Home
