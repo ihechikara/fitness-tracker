d54be4c869f2a2170b9b05a9d92a72254d9bbd6f
Ihechikara <ihechivinabba@gmail.com>
2023-05-20 18:10:42 +0100

    feat: set up frontend
    - Install React with Vite
    - Install React Router
    - Create Navbar component
    - Set up basic routes and pages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+// pages
+import Home from "./pages/Home"
+
+// components
+import Navbar from "./components/Navbar"
+
+function App() {
+  
+
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar/>
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={<Home/>}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
