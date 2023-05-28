fddce5bd2b73524d03c53f7a7980bf50f500649a
Ihechikara <ihechivinabba@gmail.com>
2023-05-28 15:39:33 +0100

    feat: create sign up logic
    - create salt and hash for user credentials
    - sign up and store user credentials in DB

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -8,7 +8,16 @@ const loginUser = async (req, res) => {
 
 // signup controller
 const signupUser = async (req, res) => {
-    res.json({msg: "User signed in"})
+    const { email, password } = req.body
+
+    try {
+        const user = await User.signup(email, password)
+
+        res.status(200).json({ email, user })
+    } catch (error) {
+        res.status(400).json({ error: error.message})
+    }
+    // res.json({msg: "User signed in"})
 }
 
 module.exports = { loginUser, signupUser }
