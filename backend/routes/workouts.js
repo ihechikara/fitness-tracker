22525d5244cfe777670c47c1ac1ca302f0f6c484
Ihechikara <ihechivinabba@gmail.com>
2023-06-08 14:38:14 +0100

    feat: create middleware for routes
    - protect routes from unauthorized requests
    - validate jwt before response

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,8 +1,11 @@
 const express = require("express")
 
 const { createWorkout, getAllWorkouts, getSingleWorkout, updateWorkout, deleteWorkout } = require("../controllers/workout")
+const requireAuth = require("../middleware/requireAuth")
 
 const router = express.Router()
+
+router.use(requireAuth)
 
 router.post("/", createWorkout)
 
